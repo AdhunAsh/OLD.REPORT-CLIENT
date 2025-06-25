090acee3c89597fe32a8063c245eef6d376cd489
AdhunAsh <adhunav147@gmail.com>
2025-06-25 12:04:48 +0530

    [PATCH] upfated about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -50,6 +50,28 @@ const About = () => {
                     </p>
                 </div>
             </div>
+            <div className="text-xl py-4">
+                <Title text1={'WHY'} text2={'CHOOSE US'} />
+            </div>
+
+            <div className="flex flex-col md:flex-row text-sm mb-20">
+                <div className="border px-10 md:px-16 py-8 s,:py-20 flex flex-col gap-5">
+                    <b>Quality Assurance: </b>
+                    <p className="text-gray-600">We meticulously select and vet each product to ensure it meets our quality standards. Your satisfaction is our priority</p>
+                </div>
+
+                <div className="border px-10 md:px-16 py-8 s,:py-20 flex flex-col gap-5">
+                    <b>Convenience: </b>
+                    <p className="text-gray-600">With our user-friendly interface and hassle-free ordering process, shopping has never been easier.</p>
+                </div>
+
+                <div className="border px-10 md:px-16 py-8 s,:py-20 flex flex-col gap-5">
+                    <b>Exceptional Customer Service: </b>
+                    <p className="text-gray-600">Our team of dedicated professionals is here to assist ou the way, ensuring your satisfaction is our top priority.</p>
+                </div>
+                
+            </div>
+
         </div>
     );
 };
