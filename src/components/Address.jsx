55ebb7966cced0e885225264ff11c37a9062e3ca
AdhunAsh <adhunav147@gmail.com>
2025-07-09 23:28:07 +0530

    [FEAT] added a component for saving address

---
+++ src/components/Address.jsx
@@ -0,0 +1,170 @@
+import React from "react";
+
+const Address = () => {
+    const { getToken } = useAuth();
+
+    const [form, setForm] = useState({
+        first_name: "",
+        Last_name: "",
+        address_line1: "",
+        street: "",
+        postal_code: "",
+        city: "",
+        state: "",
+        phone_number: "",
+    });
+
+    const handleChange = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value });
+    };
+
+    const checkProfile = async () => {
+        try {
+            const token = await getToken();
+            const response = await axiosInstance.get("/address/", {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            console.log(response);
+            if (response.data) {
+                const {
+                    first_name,
+                    Last_name,
+                    address_line1,
+                    street,
+                    postal_code,
+                    city,
+                    state,
+                    phone_number,
+                } = response.data;
+
+                console.log(response.data);
+                setForm({
+                    first_name: first_name || "",
+                    Last_name: Last_name || "",
+                    address_line1: address_line1 || "",
+                    street: street || "",
+                    postal_code: postal_code || "",
+                    city: city || "",
+                    state: state || "",
+                    phone_number: phone_number || "",
+                });
+            }
+        } catch (error) {
+            console.log("Error fetching profile:", error);
+        }
+    };
+
+    useEffect(() => {
+        checkProfile();
+    }, []);
+
+    useEffect(() => {
+        console.log("Updated form:", form);
+    }, [form]);
+
+    const saveProfile = async () => {
+        const formData = new FormData();
+        Object.entries(form).forEach(([key, value]) => {
+            formData.append(key, value);
+        });
+
+        try {
+            const token = await getToken();
+            await axiosInstance.post(`/address/`, formData, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            toast.success("Profile saved successfully...");
+        } catch (err) {
+            console.log(err);
+            toast.error("Failed to save profile!");
+        }
+    };
+
+    return (
+        <div className="flex flex-col gap-4 w-full sm:max-w-[480px]">
+            <div className="flex gap-3">
+                <input
+                    name="first_name"
+                    value={form.first_name}
+                    onChange={handleChange}
+                    className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+                    type="text"
+                    placeholder="First name"
+                />
+                <input
+                    name="Last_name"
+                    value={form.Last_name}
+                    onChange={handleChange}
+                    className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+                    type="text"
+                    placeholder="Last name"
+                />
+            </div>
+            <input
+                name="address_line1"
+                value={form.address_line1}
+                onChange={handleChange}
+                className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+                type="text"
+                placeholder="Flat, House No, Building, Company, Apartment"
+            />
+            <div className="flex gap-3">
+                <input
+                    name="street"
+                    value={form.street}
+                    onChange={handleChange}
+                    className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+                    type="text"
+                    placeholder="Street"
+                />
+                <input
+                    name="postal_code"
+                    value={form.postal_code}
+                    onChange={handleChange}
+                    className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+                    type="number"
+                    placeholder="PinCode"
+                />
+            </div>
+            <div className="flex gap-3">
+                <input
+                    name="city"
+                    value={form.city}
+                    onChange={handleChange}
+                    className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+                    type="text"
+                    placeholder="City"
+                />
+                <input
+                    name="state"
+                    value={form.state}
+                    onChange={handleChange}
+                    className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+                    type="text"
+                    placeholder="State"
+                />
+            </div>
+            <input
+                name="phone_number"
+                value={form.phone_number}
+                onChange={handleChange}
+                className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+                type="number"
+                placeholder="Phone"
+            />
+
+            <button
+                onClick={saveProfile}
+                className="bg-slate-600 text-white border border-gray-300 rounded py-2 px-3.5 w-full active:bg-slate-500"
+            >
+                Update
+            </button>
+        </div>
+    );
+};
+
+export default Address;
--- src/pages/PlaceOrder.jsx
+++ src/pages/PlaceOrder.jsx
@@ -5,127 +5,16 @@ import { ShopContext } from "../context/ShopContext";
 import { useState } from "react";
 import PaymentButton from "../components/payment";
 import { toast } from "react-toastify";
+import Address from "../components/Address";
+
 
 const PlaceOrder = () => {
     const { navigate, getCartAmount } = useContext(ShopContext);
 
-    const [form, setForm] = useState({
-        firstName: "",
-        lastName: "",
-        email: "",
-        address: "",
-        street: "",
-        pincode: "",
-        city: "",
-        state: "",
-        phone: "",
-    });
-
-    const handleChange = (e) => {
-        setForm({ ...form, [e.target.name]: e.target.value });
-    };
-
-    // const handlePlaceOrder = async () => {
-    //   const formData = new FormData();
-    //   Object.entries(form).forEach(([key, value]) => {
-    //     formData.append(key, value);
-    //   });
-
-    //   try {
-    //     await axiosInstance.post('/api/orders/', formData);
-    //     toast.success("order successfully placed")
-    //     navigate("/orders");
-    //   } catch (err) {
-    //     toast.error("Order failed!");
-    //   }
-    // };
-
     return (
         <div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t">
             {/* left side */}
-            <div className="flex flex-col gap-4 w-full sm:max-w-[480px]">
-                <div className="text-xl sm:text-2xl my-3">
-                    <Title text1={"DELIVERY"} text2={"INFORMATION"} />
-                </div>
-                <div className="flex gap-3">
-                    <input
-                        name="firstName"
-                        value={form.firstName}
-                        onChange={handleChange}
-                        className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
-                        type="text"
-                        placeholder="First name"
-                    />
-                    <input
-                        name="lastName"
-                        value={form.lastName}
-                        onChange={handleChange}
-                        className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
-                        type="text"
-                        placeholder="Last name"
-                    />
-                </div>
-                <input
-                    name="email"
-                    value={form.email}
-                    onChange={handleChange}
-                    className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
-                    type="email"
-                    placeholder="Email Address"
-                />
-                <input
-                    name="address"
-                    value={form.address}
-                    onChange={handleChange}
-                    className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
-                    type="text"
-                    placeholder="Flat, House No, Building, Company, Apartment"
-                />
-                <div className="flex gap-3">
-                    <input
-                        name="street"
-                        value={form.street}
-                        onChange={handleChange}
-                        className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
-                        type="text"
-                        placeholder="Street"
-                    />
-                    <input
-                        name="pincode"
-                        value={form.pincode}
-                        onChange={handleChange}
-                        className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
-                        type="number"
-                        placeholder="PinCode"
-                    />
-                </div>
-                <div className="flex gap-3">
-                    <input
-                        name="city"
-                        value={form.city}
-                        onChange={handleChange}
-                        className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
-                        type="text"
-                        placeholder="City"
-                    />
-                    <input
-                        name="state"
-                        value={form.state}
-                        onChange={handleChange}
-                        className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
-                        type="text"
-                        placeholder="State"
-                    />
-                </div>
-                <input
-                    name="phone"
-                    value={form.phone}
-                    onChange={handleChange}
-                    className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
-                    type="number"
-                    placeholder="Phone"
-                />
-            </div>
+            <Address />
 
             {/* right side */}
             <div className="mt-8">
