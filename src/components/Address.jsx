9554c241ecd9e9bce5b2a8745fde825202f8e6c4
AdhunAsh <adhunav147@gmail.com>
2025-07-09 23:39:53 +0530

    [PATCH] added toastify

--- src/components/Address.jsx
+++ src/components/Address.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useAuth } from "@clerk/clerk-react";
+import axiosInstance from "../axios";
+import { toast } from "react-toastify";
 
 const Address = () => {
     const { getToken } = useAuth();
