f026004d251ccbb05030750311cfbf6d819a0c59
AdhunAsh <adhunav147@gmail.com>
2025-07-15 03:06:29 +0530

    [PATCH] solved the confirm button in payment page

--- src/components/ShippingAddress.jsx
+++ src/components/ShippingAddress.jsx
@@ -56,9 +56,8 @@ const ShippingAddress = ({ onConfirm }) => {
                     state: state || "",
                     phone_number: phone_number || "",
                 });
-            }
-            else {
-                toast.error('fill delivery information')
+            } else {
+                toast.error("fill delivery information");
             }
         } catch (error) {
             console.log("Error fetching profile:", error);
@@ -123,97 +122,95 @@ const ShippingAddress = ({ onConfirm }) => {
                 <div className="text-xl sm:text-2xl my-3">
                     <Title text1={"DELIVERY"} text2={"INFORMATION"} />
                 </div>
-                    <div className="flex gap-3">
-                        <input
-                            name="first_name"
-                            value={form.first_name}
-                            onChange={handleChange}
-                            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
-                            type="text"
-                            placeholder="First name"
-                        />
-                        <input
-                            name="Last_name"
-                            value={form.Last_name}
-                            onChange={handleChange}
-                            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
-                            type="text"
-                            placeholder="Last name"
-                        />
-                    </div>
+                <div className="flex gap-3">
                     <input
-                        name="address_line1"
-                        value={form.address_line1}
+                        name="first_name"
+                        value={form.first_name}
                         onChange={handleChange}
                         className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
                         type="text"
-                        placeholder="Flat, House No, Building, Company, Apartment"
+                        placeholder="First name"
                     />
-                    <div className="flex gap-3">
-                        <input
-                            name="street"
-                            value={form.street}
-                            onChange={handleChange}
-                            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
-                            type="text"
-                            placeholder="Street"
-                        />
-                        <input
-                            name="postal_code"
-                            value={form.postal_code}
-                            onChange={(e) => {
-                                const input = e.target.value;
-                                if (/^\d{0,6}$/.test(input)) {
-                                    setForm({ ...form, postal_code: input });
-                                    setIsDirty(true);
-                                }
-                            }}
-                            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
-                            type="text"
-                            placeholder="PinCode"
-                        />
-                    </div>
-                    <div className="flex gap-3">
-                        <input
-                            name="city"
-                            value={form.city}
-                            onChange={handleChange}
-                            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
-                            type="text"
-                            placeholder="City"
-                        />
-                        <input
-                            name="state"
-                            value={form.state}
-                            onChange={handleChange}
-                            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
-                            type="text"
-                            placeholder="State"
-                        />
-                    </div>
                     <input
-                        name="phone_number"
-                        value={form.phone_number}
+                        name="Last_name"
+                        value={form.Last_name}
+                        onChange={handleChange}
+                        className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+                        type="text"
+                        placeholder="Last name"
+                    />
+                </div>
+                <input
+                    name="address_line1"
+                    value={form.address_line1}
+                    onChange={handleChange}
+                    className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+                    type="text"
+                    placeholder="Flat, House No, Building, Company, Apartment"
+                />
+                <div className="flex gap-3">
+                    <input
+                        name="street"
+                        value={form.street}
+                        onChange={handleChange}
+                        className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+                        type="text"
+                        placeholder="Street"
+                    />
+                    <input
+                        name="postal_code"
+                        value={form.postal_code}
                         onChange={(e) => {
                             const input = e.target.value;
-                            if (/^\d{0,10}$/.test(input)) {
-                                setForm({ ...form, phone_number: input });
+                            if (/^\d{0,6}$/.test(input)) {
+                                setForm({ ...form, postal_code: input });
                                 setIsDirty(true);
                             }
                         }}
                         className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
                         type="text"
-                        placeholder="Phone"
+                        placeholder="PinCode"
                     />
+                </div>
+                <div className="flex gap-3">
+                    <input
+                        name="city"
+                        value={form.city}
+                        onChange={handleChange}
+                        className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+                        type="text"
+                        placeholder="City"
+                    />
+                    <input
+                        name="state"
+                        value={form.state}
+                        onChange={handleChange}
+                        className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+                        type="text"
+                        placeholder="State"
+                    />
+                </div>
+                <input
+                    name="phone_number"
+                    value={form.phone_number}
+                    onChange={(e) => {
+                        const input = e.target.value;
+                        if (/^\d{0,10}$/.test(input)) {
+                            setForm({ ...form, phone_number: input });
+                            setIsDirty(true);
+                        }
+                    }}
+                    className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+                    type="text"
+                    placeholder="Phone"
+                />
 
-                    {isDirty && (
-                        <button
-                            onClick={saveProfile}
-                            className="bg-black w-full text-white text-[12px] px-4 py-2 sm:text-sm my-8 sm:px-8 sm:py-3 hover:bg-gray-700"
-                        >
-                            CONFIRM ADDRESS
-                        </button>
-                    )}
+                <button
+                    onClick={saveProfile}
+                    className="bg-black w-full text-white text-[12px] px-4 py-2 sm:text-sm my-8 sm:px-8 sm:py-3 hover:bg-gray-700"
+                >
+                    CONFIRM ADDRESS
+                </button>
             </div>
         </div>
     );
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,10 +1,40 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from '../components/Title'
+import { useAuth } from '@clerk/clerk-react';
+import axiosInstance from '../axios';
+import { toast } from 'react-toastify';
 
 const Orders = () => {
 
-  const { products, currency } = useContext(ShopContext)
+  const { products, currency } = useContext(ShopContext);
+  const { getToken } = useAuth();
+  const [ orders, setorders ] = useState([])
+
+  const orderData = async () => {
+    try {
+      
+      const token = await getToken();
+      const response = await axiosInstance.get('/order-list/', { headers: {
+        Authorization: `bearer ${token}`
+      }})
+      if (response.data){
+        console.log(response.data);
+        setorders(response.data)
+      }
+  
+    } catch (error) {
+      console.log(error);
+      toast.error(error)
+    }
+
+  }
+
+  useEffect(()=>{
+    orderData();
+  },[])
+
+
   return (
     <div className='border-t pt-16'>
       <div className='text-2xl'>
@@ -12,14 +42,14 @@ const Orders = () => {
       </div>
       <div>
         {
-          products.slice(1,4).map((item, index)=>(
+          orders.map((item, index)=>(
             <div key={index} className='py-4 border-t border-b text-gray-700 flex flex-col md:flex-row md:items-center md:justify-between gap-4'>
               <div className='flex items-start gap-6 text-sm'>
-                <img className='w-16 sm:w-20' src= {item.images[0].image} alt="" />
+                <img className='w-16 sm:w-20' src= {item.items.image[0]} alt="" />
                 <div>
-                  <p className='sm:text-base font-medium'>{item.name}</p>
+                  <p className='sm:text-base font-medium'>{item.items.product}</p>
                   <div className='flex items-center gap-3 mt-2 text-base text-gray-700'>
-                    <p className='text-lg'>{currency}{item.price}.00</p>
+                    <p className='text-lg'>{currency}{(item.items.price / 100).toFixed(2)}</p>
                     <p>Quantity: 1</p>
                     <p>Size: M</p>
                   </div>
@@ -32,8 +62,7 @@ const Orders = () => {
                   <p className='text-sm md:text-base'>Ready to ship</p>
                 </div>
               </div>
-
-            </ div>
+            </div>
           ))
         }
       </div>
@@ -42,3 +71,4 @@ const Orders = () => {
 }
 
 export default Orders
+
